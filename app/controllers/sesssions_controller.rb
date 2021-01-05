e5b0eaadbd44aab8d3178ebf4ad66073b75c76a1
Sean <skabe89@gmail.com>
2021-01-04 23:17:30 -0500

    added login and lgout

--- app/controllers/sesssions_controller.rb
+++ app/controllers/sesssions_controller.rb
@@ -13,4 +13,23 @@ class SessionsController < ApplicationController
       redirect '/signup'
     end
   end
+
+  get '/login' do
+    erb :'sessions/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect '/'
+    else
+      redirect '/'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
 end
